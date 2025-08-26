19934d0b58b076718e0b0f3db97a6494e2389ae9
Kishanr7 <kishanrekhadia79@gmail.com>
2025-08-26 15:55:16 +0530

    daily ques 26-08

---
+++ Medium/restaurant_growth_1321.sql
@@ -0,0 +1,21 @@
+SELECT 
+    visited_on,
+    amount,
+    average_amount
+FROM  (
+    SELECT 
+        visited_on,
+        SUM(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+    CAST(AVG(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS DECIMAL(10,2)) AS average_amount
+    FROM (
+        SELECT 
+            visited_on,
+            SUM(amount) as amount
+        FROM customer
+        GROUP BY visited_on
+    ) daily
+) t 
+WHERE visited_on >= (
+    SELECT MIN(visited_on) + INTERVAL 6 day FROM customer
+)
+ORDER BY visited_on
